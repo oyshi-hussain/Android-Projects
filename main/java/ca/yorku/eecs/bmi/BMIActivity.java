357ddeccd507f7b0e46c5550ace8002eafc93654
oyshi-hussain <oyshihussain21@gmail.com>
2021-01-10 20:33:33 -0500

    initial push

---
+++ main/java/ca/yorku/eecs/bmi/BMIActivity.java
@@ -0,0 +1,35 @@
+package ca.yorku.eecs.bmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class BMIActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bmi_layout);
+    }
+
+    public void buttonClicked(View v)
+    {
+        EditText weightView = (EditText) findViewById(R.id.weightBox);
+        String weight = weightView.getText().toString();
+        EditText heightView = (EditText) findViewById(R.id.heightBox);
+        String height = heightView.getText().toString();
+
+        BMIModel myModel = new BMIModel(weight, height);
+        String myBMI = myModel.getBMI();
+        String weightT = myModel.toPound();
+        String heightT = myModel.toFeetInch();
+
+        String out = "Your weight is " + weightT + " lb, your Height is " + heightT + ", and your BMI is " + myBMI;
+
+        ((TextView) findViewById(R.id.answer)).setText(out);
+    }
+}
